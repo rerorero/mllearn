a6f31de960efded5abde53b04395ffa3000c510c
rerorero <nato9598@hotmail.co.jp>
2024-02-06 08:20:55 +0900

    trainer tochuu

---
+++ transformer/src/transformer/path.py
@@ -0,0 +1,14 @@
+from os.path import abspath, dirname, join
+
+PROJECT_ROOT = join(abspath(dirname(__file__)), "../..")
+CORPUS_PATH = join(PROJECT_ROOT, "corpus")
+
+KFTT_CORPUS_PATH = join(CORPUS_PATH, "kftt-data-1.0", "data")
+
+KFTT_TOK_CORPUS_PATH = join(KFTT_CORPUS_PATH, "tok")
+
+
+PICKLES_PATH = join(PROJECT_ROOT, "pickles")
+NN_MODEL_PICKLES_PATH = join(PICKLES_PATH, "nn")
+FIGURE_PATH = join(PROJECT_ROOT, "figure")
+
